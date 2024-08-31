d8b34fc67c45a9010d66784e54474bdd6096181d
Sushant Sharma <sushant20.sharma00@gmail.com>
2024-08-31 10:41:54 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <div className="flex flex-col justify-center items-center h-screen gap-10">
       <h1 className="text-4xl font-bold">Roast your Instagram</h1>
       <Link
-        href="https://api.instagram.com/oauth/authorize?client_id=380484498248881&redirect_uri=https://instagram-profile-test.vercel.app/insta-profile/&scope={scope}&response_type=code"
+        href="https://api.instagram.com/oauth/authorize?client_id=380484498248881&redirect_uri=https://instagram-profile-test.vercel.app/insta-profile/&scope=user_profile,user_media&response_type=code"
         className="py-2 px-8 rounded-lg bg-black text-white"
       >
         Start
