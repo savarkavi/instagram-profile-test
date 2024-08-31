76b568bbbc2631a587f537ab4e5e78c29f40877b
Sushant Sharma <sushant20.sharma00@gmail.com>
2024-08-31 10:58:29 +0530

    updated

--- src/app/insta-profile/page.tsx
+++ src/app/insta-profile/page.tsx
@@ -3,6 +3,7 @@
 import axios from "axios";
 import React, { useEffect, useState, Suspense } from "react";
 import { useSearchParams } from "next/navigation";
+import { getProfile } from "../../../actions";
 
 const InstaProfile = () => {
   const searchParams = useSearchParams();
@@ -11,27 +12,9 @@ const InstaProfile = () => {
 
   useEffect(() => {
     if (code) {
-      const getProfile = async () => {
-        try {
-          const res =
-            await axios.post(` https://api.instagram.com/oauth/access_token \
-  -F client_id=380484498248881 \
-  -F client_secret=ef9ee33cf46ace3955bf3e60c68f199e \
-  -F grant_type=authorization_code \
-  -F redirect_uri=https://instagram-profile-test.vercel.app/insta-profile/ \
-  -F code=${code}`);
+      const res = getProfile(code);
 
-          const profile = await axios.get(
-            `https://graph.instagram.com/me?fields=id,username&access_token=${res.data.access_token}`
-          );
-
-          console.log(profile);
-        } catch (error) {
-          console.log(error);
-        }
-      };
-
-      getProfile();
+      console.log(res);
     }
   }, [code]);
 
