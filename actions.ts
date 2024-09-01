08b57d1c6b65422664e90a9ec9ada1e067b4ecbb
Sushant Sharma <sushant20.sharma00@gmail.com>
2024-09-01 06:53:00 +0530

    updated

--- actions.ts
+++ actions.ts
@@ -5,7 +5,21 @@ import axios from "axios";
 export const getProfile = async (code: string) => {
   try {
     const res = await axios.post(
-      `https://api.instagram.com/oauth/access_token?client_id=380484498248881&client_secret=ef9ee33cf46ace3955bf3e60c68f199e&grant_type=authorization_code&redirect_uri=https://instagram-profile-test.vercel.app/insta-profile/&code=${code}`
+      "https://api.instagram.com/oauth/access_token",
+      null,
+      {
+        params: {
+          client_id: "380484498248881",
+          client_secret: "ef9ee33cf46ace3955bf3e60c68f199e",
+          grant_type: "authorization_code",
+          redirect_uri:
+            "https://instagram-profile-test.vercel.app/insta-profile/",
+          code: code,
+        },
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
     );
 
     const profile = await axios.get(
